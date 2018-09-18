6d2a70835e1b7a7fed830ae7da6c48f5239abb64
Asus <Asus@Raka-Rehardsyah>
2018-09-18 12:17:47 +0700

    Final Modul 1

--- Modul/src/Modul1/Tugas3.java
+++ Modul/src/Modul1/Tugas3.java
@@ -1,9 +1,16 @@
 package Modul1;
-
+import java.util.Scanner;
 public class Tugas3 {
 
     public static void main(String[] args) {
-
+        String nama, nis, tempatlahir, tangallahir, jeniskelamin, alamatdimalang, mottohidup;
+        Scanner masukan = new Scanner(System.in);
+        System.out.println("Nama          : ");
+        nama = masukan.next();
+        System.out.println("NIS          : ");
+        nis = masukan.next();
+        System.out.println("Tempatlahir         :");
+        tempatlahir = masukan.next();
     }
 
 }
