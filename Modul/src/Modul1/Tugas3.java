24433fa1d0d08f22a38793eeb4f3f5ee0f8c70c6
Asus <Asus@Raka-Rehardsyah>
2018-09-18 13:15:45 +0700

    Final Modul 1

--- Modul/src/Modul1/Tugas3.java
+++ Modul/src/Modul1/Tugas3.java
@@ -5,12 +5,20 @@ public class Tugas3 {
     public static void main(String[] args) {
         String nama, nis, tempatlahir, tangallahir, jeniskelamin, alamatdimalang, mottohidup;
         Scanner masukan = new Scanner(System.in);
-        System.out.println("Nama          : ");
+        System.out.println("Nama : ");
         nama = masukan.next();
-        System.out.println("NIS          : ");
+        System.out.println("NIS : ");
         nis = masukan.next();
-        System.out.println("Tempatlahir         :");
+        System.out.println("Tempatlahir :");
         tempatlahir = masukan.next();
+        System.out.println("Tanggal Lahir : ");
+        tangallahir = masukan.next();
+        System.out.println("Jenis Kelamin : ");
+        jeniskelamin = masukan.next();
+        System.out.println("Alamat di Malang : ");
+        alamatdimalang = masukan.next();
+        System.out.println("Motto Hidup : ");
+        mottohidup = masukan.next();
     }
 
 }
